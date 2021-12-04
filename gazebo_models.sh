3a9f07874206702ffb637a62e5310441609bf650
hibetterheyj <he-yujie@outlook.com>
2021-12-05 00:03:13 +0100

    first commit

---
+++ gazebo_models.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#TODO: add code to check if $HOME/.gazebo/models/ exists
+
+# Download all model archive files
+wget -l 2 -nc -r "http://models.gazebosim.org/" --accept gz
+
+# This is the folder into which wget downloads the model archives
+cd "models.gazebosim.org"
+
+# Extract all model archives
+for i in *
+do
+  tar -zvxf "$i/model.tar.gz"
+done
+
+# Copy extracted files to the local model folder
+cp -vfR * "$HOME/.gazebo/models/"
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Shell scripts
+
+- `get-fonts.sh`: download additional fonts for ubuntu
+- `gazebo_models.sh`: download models for gazebo
+    - TODO: add code to check if $HOME/.gazebo/models/ exists
+    - source: [2018-01-05:如何下载Gazebo模型并安装](https://www.jianshu.com/p/ca229733c22a)
