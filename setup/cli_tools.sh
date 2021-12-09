51696ba653d3b0a358067a534261847b25122522
hibetterheyj <he-yujie@outlook.com>
2021-12-09 03:58:54 +0100

    update pip, copyq, trash-cli in cli_tools.sh

--- setup/cli_tools.sh
+++ setup/cli_tools.sh
@@ -16,10 +16,15 @@ source ./scriptutils.sh
 
 sudo apt update
 
+## dev
 # git
 check_install git
+# python3-pip
+check_install python3-pip
 
-# unzip
+## files
+# zip, unzip
+check_install zip
 check_install unzip
 
 # tldr
@@ -28,16 +33,23 @@ check_install tldr
 # tmux
 check_install tmux
 
+# net-tools (ifconfig)
+check_install net-tools
+
 # htop
 check_install htop
-
-# gpustat
-check_install gpustat
 
 # ffmpeg
 check_install ffmpeg
 echo "Video Generation Test from https://www.bogotobogo.com/FFMpeg/ffmpeg_video_test_patterns_src.php"
 echo "ffmpeg -f lavfi -i testsrc=duration=5.3:size=qcif:rate=10 testsrc2.mp4"
+
+# clipboard manager: copyq
+check_install copyq
+
+# trash-cli
+# https://github.com/andreafrancia/trash-cli
+check_install trash-cli
 
 # vim
 # sudo apt install vim -y
